9c2fb17a136a855b480538044a87cbbf9a11fa12
Rishabh Gupta <belikerishabh@gmail.com>
2024-08-20 23:33:34 +0530

    feat: snapshot testing added

--- tests/dfn.test.ts
+++ tests/dfn.test.ts
@@ -1,11 +1,15 @@
+import { su } from "@tscircuit/soup-util"
 import test from "ava"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
-import { su } from "@tscircuit/soup-util"
 
 test("dfn8_w5.3mm_p1.27mm", async (t) => {
-  const { fp, logSoup } = await getTestFixture(t)
+  const { fp, logSoup, pcbToSvg } = await getTestFixture(t)
   const soup = fp.string("dfn8_w5.3mm_p1.27mm").soup()
 
   t.is(su(soup).pcb_smtpad.list().length, 8)
+
+  const svg = pcbToSvg(soup)
+  t.snapshot(svg)
+
   await logSoup(soup)
 })
--- tests/fixtures/get-test-fixture.ts
+++ tests/fixtures/get-test-fixture.ts
@@ -2,6 +2,7 @@ import type { ExecutionContext } from "ava"
 import { fp } from "../../src"
 import { logSoup } from "@tscircuit/log-soup"
 import type { AnySoupElement } from "@tscircuit/soup"
+import { pcbSoupToSvg, soupToSvg } from "circuit-to-svg";
 
 export const getTestFixture = async (t: ExecutionContext) => {
   return {
@@ -15,6 +16,12 @@ export const getTestFixture = async (t: ExecutionContext) => {
       if (process.env.CI) return
       if (process.env.FULL_RUN) return
       return logSoup(`footprinter: ${t.title} ${prefix}`, soup)
+    },
+    pcbToSvg: (soup: AnySoupElement[]) => {
+      return pcbSoupToSvg(soup)
+    },
+    schematicToSvg: (soup: AnySoupElement[]) => {
+      return soupToSvg(soup)
     },
   }
 }
