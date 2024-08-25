0d862854acb1b3a25d100eebeb2cea7ef4b93ed9
Anas Sarkiz <anassarkiz@Anass-MacBook-Air.local>
2024-08-25 21:22:38 +0200

    Fixed a bug in soic fn
    soic does not account for pin width when calculating pitch

--- tests/tssop.test.ts
+++ tests/tssop.test.ts
@@ -6,6 +6,14 @@ test("tssop", async (t) => {
   const { fp, logSoup, snapshotSoup } = await getTestFixture(t)
   const soup = fp.string("tssop8_w5.3mm_p1.27mm").soup()
 
-  await snapshotSoup(soup)
+  snapshotSoup(soup)
+  t.pass()
+})
+
+test("tssop20_w6.5mm_p0.65mm", async (t) => {
+  const { fp, logSoup, snapshotSoup } = await getTestFixture(t)
+  const soup = fp.string("tssop20_w6.5mm_p0.65mm").soup()
+
+  snapshotSoup(soup)
   t.pass()
 })
