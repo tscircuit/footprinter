2ee3ee342d928a2788beda6b2f74d74a65845df1
Anas sarkiz <anassoftwaredev@gmail.com>
2024-08-25 23:02:54 +0200

    Fixed a bug in soic fn
    soic does not account for pin width when calculating pitch

--- tests/dfn.test.ts
+++ tests/dfn.test.ts
@@ -9,3 +9,11 @@ test("dfn8_w5.3mm_p1.27mm", async (t) => {
   t.is(su(soup).pcb_smtpad.list().length, 8)
   snapshotSoup(soup)
 })
+
+test("dfn8_w2_h3_p0.5mm", async (t) => {
+  const { fp, logSoup, snapshotSoup } = await getTestFixture(t)
+  const soup = fp.string("dfn8_w2_h3_p0.5mm").soup()
+
+  t.is(su(soup).pcb_smtpad.list().length, 8)
+  snapshotSoup(soup)
+})
