7cc0266227d83f98401989100e57239f053fcd92
Anas sarkiz <anassoftwaredev@gmail.com>
2024-08-26 14:52:50 +0200

    Added SOT563 fn and test

--- tests/sot563.test.ts
+++ tests/sot563.test.ts
@@ -2,10 +2,10 @@ import test from "ava"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
 import { su } from "@tscircuit/soup-util"
 
-test("dfn8_w5.3mm_p1.27mm", async (t) => {
+test("sot563", async (t) => {
   const { fp, logSoup, snapshotSoup } = await getTestFixture(t)
-  const soup = fp.string("dfn8_w5.3mm_p1.27mm").soup()
+  const soup = fp.string("sot563").soup()
 
-  t.is(su(soup).pcb_smtpad.list().length, 8)
   snapshotSoup(soup)
+  t.pass()
 })
