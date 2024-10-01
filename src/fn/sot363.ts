9fc365c414925047ef31b2766c6c7af615e1f5ec
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-10-01 22:41:50 +0530

    format fix

--- src/fn/sot363.ts
+++ src/fn/sot363.ts
@@ -1,21 +1,21 @@
-import type { AnySoupElement } from "@tscircuit/soup";
-import { extendSoicDef, soicWithoutParsing } from "./soic";
-import type { z } from "zod";
+import type { AnySoupElement } from "@tscircuit/soup"
+import { extendSoicDef, soicWithoutParsing } from "./soic"
+import type { z } from "zod"
 
-export const sot363_def = extendSoicDef({});
+export const sot363_def = extendSoicDef({})
 
 export const sot363 = (
-  raw_params: z.input<typeof sot363_def>
+  raw_params: z.input<typeof sot363_def>,
 ): { circuitJson: AnySoupElement[]; parameters: any } => {
   const parameters = sot363_def.parse({
     fn: "sot363",
     num_pins: 6,
     w: 1.25,
     p: 0.65,
-  });
+  })
 
   return {
     circuitJson: soicWithoutParsing(parameters),
     parameters,
-  };
-};
+  }
+}
--- tests/sot363.test.ts
+++ tests/sot363.test.ts
@@ -1,11 +1,11 @@
-import test from "ava";
-import { getTestFixture } from "tests/fixtures/get-test-fixture";
+import test from "ava"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
 
 test("sot363", async (t) => {
-  const { fp, logSoup, snapshotSoup } = await getTestFixture(t);
-  const soup = fp.string("sot363").soup();
+  const { fp, logSoup, snapshotSoup } = await getTestFixture(t)
+  const soup = fp.string("sot363").soup()
 
-  await logSoup(soup);
-  snapshotSoup(soup);
-  t.pass();
-});
+  await logSoup(soup)
+  snapshotSoup(soup)
+  t.pass()
+})
