df37a571c58004809394f36320dccbb06a746882
Anas sarkiz <anassoftwaredev@gmail.com>
2024-10-01 20:38:57 +0200

    formatted

--- src/fn/dip.ts
+++ src/fn/dip.ts
@@ -1,4 +1,8 @@
-import type { AnySoupElement, PcbFabricationNoteText, PcbSilkscreenPath } from "@tscircuit/soup"
+import type {
+  AnySoupElement,
+  PcbFabricationNoteText,
+  PcbSilkscreenPath,
+} from "@tscircuit/soup"
 import { u_curve } from "../helpers/u-curve"
 import { platedhole } from "../helpers/platedhole"
 import { z } from "zod"
@@ -116,12 +120,14 @@ export const dip = (raw_params: {
       { x: -sw / 2, y: -sh / 2 },
     ],
     type: "pcb_silkscreen_path",
-    stroke_width: 0.1
+    stroke_width: 0.1,
   }
   const silkscreenPins: PcbFabricationNoteText[] = []
   for (let i = 0; i < parameters.num_pins; i++) {
     const isLeft = i < parameters.num_pins / 2
-    const pinLabelX = isLeft ? -parameters.w / 2 - parameters.p / 2 - 0.2 : parameters.p / 2 + parameters.w / 2 + 0.2
+    const pinLabelX = isLeft
+      ? -parameters.w / 2 - parameters.p / 2 - 0.2
+      : parameters.p / 2 + parameters.w / 2 + 0.2
     const pinLabelY = isLeft
       ? (-sh + 1.6) / 2 + i * parameters.p
       : (-sh + 1.6) / 2 + (i - parameters.num_pins / 2) * parameters.p
