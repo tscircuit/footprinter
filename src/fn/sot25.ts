d8b5c0770eaafb9db639711b4f0936515d4f6a6e
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-10-03 11:52:46 +0530

    creating sot25

---
+++ src/fn/sot25.ts
@@ -0,0 +1,25 @@
+import type { AnySoupElement } from "@tscircuit/soup";
+import { extendSoicDef, soicWithoutParsing } from "./soic";
+import type { z } from "zod";
+
+export const sot25_def = extendSoicDef({});
+
+export const sot25 = (
+  raw_params: z.input<typeof sot25_def>
+): { circuitJson: AnySoupElement[]; parameters: any } => {
+  const parameters = sot25_def.parse({
+    fn: "sot25",
+    num_pins: 5,
+    w: 3.2,
+    h: 3.35,
+    p: 0.95,
+    pw: 0.55,
+    pl: 0.8,
+    legoutside: true,
+  });
+
+  return {
+    circuitJson: soicWithoutParsing(parameters),
+    parameters,
+  };
+};
