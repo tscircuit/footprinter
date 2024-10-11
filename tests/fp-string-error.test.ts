dac01ac5a35fa88cad4aca96b959cce02a9c6cc3
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-10-11 10:07:16 +0530

    fix: expected error in fp-string-error.test.ts

--- tests/fp-string-error.test.ts
+++ tests/fp-string-error.test.ts
@@ -1,11 +1,21 @@
 import { describe, expect, it } from "bun:test"
+import { isNotNull } from "../src/helpers/is-not-null"
 import { getTestFixture } from "./fixtures" // Adjust path based on your structure
 
 describe("FP String Error Tests", () => {
   it("should throw an error for invalid footprint function", async () => {
     const { fp } = await getTestFixture("slop1")
 
-    const invalidFootprint = "invalidFunction"
+    const invalidFootprint = "trianglularsmt4"
+    const invalidFootprintParts = invalidFootprint
+      .split("_")
+      .map((s) => {
+        const m = s.match(/([a-z]+)([\(\d\.\+\?].*)?/)
+        const [_, fn, v] = m ?? []
+        if (v?.includes("?")) return null
+        return { fn: m?.[1]!, v: m?.[2]! }
+      })
+      .filter(isNotNull)
 
     try {
       fp.string(invalidFootprint).soup()
@@ -13,9 +23,8 @@ describe("FP String Error Tests", () => {
     } catch (error) {
       expect(error).toBeInstanceOf(Error)
 
-      expect(error.message).toContain("Function not found for footprinter")
-      expect(error.message).toContain(
-        "Specify a valid function like .dip, .lr, .p, etc.",
+      expect(error.message).toEqual(
+        `Function not found for footprinter "${invalidFootprintParts[0]?.fn}". Specify a valid function like .dip, .lr, .p, etc.`,
       )
     }
   })
