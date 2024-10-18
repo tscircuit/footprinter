da8398bdf44bac69a927ca79c5847bdc85678707
seveibar <seveibar@gmail.com>
2024-10-18 13:37:12 -0700

    improve gallery generation

--- scripts/generate-gallery-page.ts
+++ scripts/generate-gallery-page.ts
@@ -20,16 +20,29 @@ function generateGalleryPage() {
     <style>
         body {
             font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+        }
+        h1 a {
+          text-decoration: none;
+          color: #007bff;
+          font-size: 24px;
+        }
+        .header {
+            margin: 12px;
+            margin-left: 64px;
+            margin-right: 64px;
+            display: flex;
+        }
+        .gallery-items {
             display: flex;
             flex-wrap: wrap;
             justify-content: center;
-            background-color: #f0f0f0;
         }
         .svg-container {
             margin: 10px;
             position: relative;
             width: 300px;
-            height: 300px;
+            height: 225px;
             background-color: white;
             box-shadow: 0 0 10px rgba(0,0,0,0.1);
         }
@@ -45,15 +58,35 @@ function generateGalleryPage() {
             padding: 5px;
             font-size: 12px;
         }
+        .github-stars {
+          margin-left: auto;
+          display: flex;
+          align-items: center;
+        }
+
+        .github-stars a img {
+          height: 24px;
+        }
     </style>
 </head>
 <body>
+    <div class="header">
+        <h1><a href="https://github.com/tscircuit/footprinter">@tscircuit/footprinter</a></h1>
+        <div style="flex-grow: 1"></div>
+        <div class="github-stars">
+          <a href="https://github.com/tscircuit/footprinter" target="_blank">
+            <img alt="GitHub stars" src="https://img.shields.io/github/stars/tscircuit/footprinter?style=social">
+          </a>
+        </div>
+    </div>
+    <div class="gallery-items">
     ${svgFiles
       .map((file) => {
-        const svgContent = fs.readFileSync(
-          path.join(snapshotsDir, file),
-          "utf-8",
-        )
+        const svgContent = fs
+          .readFileSync(path.join(snapshotsDir, file), "utf-8")
+          .replace(/width="\d+"/, 'width="300"')
+          .replace(/height="\d+"/, 'height="225" viewBox="0 0 800 600"')
+
         const title = file.replace(".snap.svg", "")
         return `
     <div class="svg-container">
@@ -62,6 +95,7 @@ function generateGalleryPage() {
     </div>`
       })
       .join("\n")}
+    </div>
 </body>
 </html>
   `
