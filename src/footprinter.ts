06dcd3bcdf20f8f9b771eed470bb365278824d26
Anas sarkiz <anassoftwaredev@gmail.com>
2024-10-19 17:58:55 +0200

    resolve conflict

--- src/footprinter.ts
+++ src/footprinter.ts
@@ -50,6 +50,9 @@ export type Footprinter = {
   dfn: (num_pins: number) => FootprinterParamsBuilder<"w" | "p">
   pinrow: (num_pins: number) => FootprinterParamsBuilder<"p" | "id" | "od">
   axial: () => FootprinterParamsBuilder<"p" | "id" | "od">
+  pushbutton: () => FootprinterParamsBuilder<
+    "tllabel" | "trlabel" | "bllabel" | "brlabel"
+  >
   stampboard: () => FootprinterParamsBuilder<
     "w" | "left" | "right" | "top" | "bottom" | "p" | "pw" | "pl" | "innerhole"
   >
