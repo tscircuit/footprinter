5875cafbdee1b063b4877be48cd7cc22ffb0fccc
Anas sarkiz <72264380+anas-sarkez@users.noreply.github.com>
2024-10-21 20:19:29 +0200

    Implemented a function to generate a stampreceiver footprint (#70)

---
+++ tests/stampreceiver.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("stampreceiver", () => {
+  const soup = fp
+    .string("stampreceiver_left20_right20_bottom3_top2_w21mm_p2.54mm")
+    .circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  expect(svgContent).toMatchSvgSnapshot(
+    import.meta.path,
+    "stampreceiver_left20_right20_bottom3_top2_w21mm_p2.54mm",
+  )
+})
