8c44fd047dd9dbc10d095d2806691f21474ad578
Anas sarkiz <72264380+anas-sarkez@users.noreply.github.com>
2024-11-08 21:10:18 +0200

    Implemented sot235 (sot23-5) footprint with testing (#78)
    * Implemented sot235 (sot23-5) footprint with testing
    
    * formated
    
    * Bun format
    
    * Refactor silkscreen and added pin 1 indecator

---
+++ tests/sot235.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("sot235", () => {
+  const soup = fp.string("sot235").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot235")
+})
