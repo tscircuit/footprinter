266615bdb2e756ee2583e5aa96270f2dbd64bd31
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-01-22 19:12:09 +0530

    fix: slop test

--- tests/pad.test.ts
+++ tests/pad.test.ts
@@ -1,12 +1,17 @@
 import { expect, test } from "bun:test"
 import { fp } from "../src/footprinter"
+import { getTestFixture } from "./fixtures/get-test-fixture"
 
-test("pad footprint", () => {
+test("pad footprint", async () => {
+  const { snapshotSoup } = await getTestFixture("pad")
   const soup = fp().pad().w(2).h(1).circuitJson()
   expect(soup).toMatchSnapshot()
+  snapshotSoup(soup)
 })
 
-test("pad footprint with different dimensions", () => {
+test("pad footprint with different dimensions", async () => {
+  const { snapshotSoup } = await getTestFixture("pad_3x2")
   const soup = fp().pad().w(3).h(2).circuitJson()
   expect(soup).toMatchSnapshot()
+  snapshotSoup(soup)
 })
