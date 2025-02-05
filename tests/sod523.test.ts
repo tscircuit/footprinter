a41cdf0d65177b297302ed9095979272e31fb2ee
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-06 02:31:04 +0530

    implement sod523 (#132)
    * add sod523
    
    * format code
    
    * remove msop
    
    * pinout pin1
    
    * add silkscreen
    
    * add snapshot
    
    * improve snapshot
    
    * improve silkscreen
    
    * improve footprint

---
+++ tests/sod523.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("sod523", () => {
+  const soup = fp.string("sod523").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sod523")
+})
