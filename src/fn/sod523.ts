70bc1b5ca6be03837ea04183b79919e56def1298
techmannih <manishchaudharyttt@gmail.com>
2025-02-06 02:06:01 +0530

    add snapshot

--- src/fn/sod523.ts
+++ src/fn/sod523.ts
@@ -22,7 +22,7 @@ export const sod523 = (
   // Define silkscreen reference text
   const silkscreenRefText: SilkscreenRef = silkscreenRef(
     0,
-    length.parse(parameters.h) / 4 + 0.4,
+    length.parse(parameters.h),
     0.3,
   )
 
