54991ce83c0008dbdbf6d8f03d736f66a3f4cc17
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-06 12:02:31 +0530

    rename circuitJson

--- tests/sod123w.test.ts
+++ tests/sod123w.test.ts
@@ -3,7 +3,7 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
 test("sod123w", () => {
-  const soup = fp.string("sod123w").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const circuitJson = fp.string("sod123w").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sod123w")
 })
