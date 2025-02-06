19e6412f6d33662ca4b8430d405efab26a9c2d42
techmannih <manishchaudharyttt@gmail.com>
2025-02-06 10:48:09 +0530

    add sod123w footprint

---
+++ tests/sod123w.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("sod123w", () => {
+  const soup = fp.string("sod123w").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sod123w")
+})
