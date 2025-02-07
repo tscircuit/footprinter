c789eb1bd22bb419302623af937257d96fcbb689
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-07 17:12:03 +0530

    rename micromelf_def

--- src/fn/micromelf.ts
+++ src/fn/micromelf.ts
@@ -4,7 +4,7 @@ import { rectpad } from "../helpers/rectpad"
 import { silkscreenRef, type SilkscreenRef } from "src/helpers/silkscreenRef"
 import { length } from "circuit-json"
 
-export const sod_def = z.object({
+export const micromelf_def = z.object({
   fn: z.string(),
   num_pins: z.literal(2).default(2),
   w: z.string().default("3.0mm"),
@@ -15,9 +15,9 @@ export const sod_def = z.object({
 })
 
 export const micromelf = (
-  raw_params: z.input<typeof sod_def>,
+  raw_params: z.input<typeof micromelf_def>,
 ): { circuitJson: AnySoupElement[]; parameters: any } => {
-  const parameters = sod_def.parse(raw_params)
+  const parameters = micromelf_def.parse(raw_params)
 
   // Define silkscreen reference text
   const silkscreenRefText: SilkscreenRef = silkscreenRef(
@@ -77,7 +77,7 @@ export const getSodCoords = (parameters: {
 }
 
 // Function to generate SOD pads
-export const sodWithoutParsing = (parameters: z.infer<typeof sod_def>) => {
+export const sodWithoutParsing = (parameters: z.infer<typeof micromelf_def>) => {
   const pads: AnySoupElement[] = []
 
   for (let i = 1; i <= parameters.num_pins; i++) {
