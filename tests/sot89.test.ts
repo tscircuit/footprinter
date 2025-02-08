e11b7938e9b06243a61fba7dd77a4773837645a8
techmannih <manishchaudharyttt@gmail.com>
2025-02-08 21:55:23 +0530

    add sot89_3

--- tests/sot89.test.ts
+++ tests/sot89.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from "bun:test"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
-test("sot89", () => {
-  const circuitJson = fp.string("sot89").circuitJson()
+test("sot89_3", () => {
+  const circuitJson = fp.string("sot89_3").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot89")
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot89_3")
 })
