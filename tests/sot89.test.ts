b9d0a242b1b0bebf79b6dee3ccc30a726198c5a4
techmannih <manishchaudharyttt@gmail.com>
2025-02-08 22:04:06 +0530

    add sot89_5

--- tests/sot89.test.ts
+++ tests/sot89.test.ts
@@ -7,3 +7,8 @@ test("sot89_3", () => {
   const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot89_3")
 })
+test("sot89_5", () => {
+  const circuitJson = fp.string("sot89_5").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot89_5")
+})
