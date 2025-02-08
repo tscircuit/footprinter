4decc3526d975a36002235760fac665b4138a3ea
techmannih <manishchaudharyttt@gmail.com>
2025-02-09 03:46:07 +0530

    add smf footprint

---
+++ src/fn/smf.ts
@@ -0,0 +1,99 @@
+import type { AnySoupElement, PcbSilkscreenPath } from "circuit-json"
+import { z } from "zod"
+import { rectpad } from "../helpers/rectpad"
+import { silkscreenRef, type SilkscreenRef } from "src/helpers/silkscreenRef"
+import { length } from "circuit-json"
+
+export const smf_def = z.object({
+  fn: z.string(),
+  num_pins: z.literal(2).default(2),
+  w: z.string().default("4.80mm"),
+  h: z.string().default("2.15mm"),
+  pl: z.string().default("1.30mm"),
+  pw: z.string().default("1.40mm"),
+  pad_spacing: z.string().default("2.9mm"),
+})
+
+export const smf = (
+  raw_params: z.input<typeof smf_def>,
+): { circuitJson: AnySoupElement[]; parameters: any } => {
+  const parameters = smf_def.parse(raw_params)
+
+  // Define silkscreen reference text
+  const silkscreenRefText: SilkscreenRef = silkscreenRef(
+    0,
+    length.parse(parameters.h)-0.5,
+    0.3,
+  )
+
+  const silkscreenLine: PcbSilkscreenPath = {
+    type: "pcb_silkscreen_path",
+    layer: "top",
+    pcb_component_id: "",
+    route: [
+      {
+        x: length.parse(parameters.pad_spacing) / 2,
+        y: length.parse(parameters.h) / 2,
+      },
+      {
+        x: -length.parse(parameters.w) / 2 - 0.1,
+        y: length.parse(parameters.h) / 2,
+      },
+      {
+        x: -length.parse(parameters.w) / 2 - 0.1,
+        y: -length.parse(parameters.h) / 2,
+      },
+      {
+        x: length.parse(parameters.pad_spacing) / 2,
+        y: -length.parse(parameters.h) / 2,
+      },
+    ],
+    stroke_width: 0.1,
+    pcb_silkscreen_path_id: "",
+  }
+
+  return {
+    circuitJson: smfWithoutParsing(parameters).concat(
+      silkscreenLine as AnySoupElement,
+      silkscreenRefText as AnySoupElement,
+    ),
+    parameters,
+  }
+}
+
+// Get coordinates for smf pads
+export const getSmfCoords = (parameters: {
+  pn: number
+  pad_spacing: number
+}) => {
+  const { pn, pad_spacing } = parameters
+
+  if (pn === 1) {
+    return { x: -pad_spacing / 2, y: 0 }
+    // biome-ignore lint/style/noUselessElse: <explanation>
+  } else {
+    return { x: pad_spacing / 2, y: 0 }
+  }
+}
+
+// Function to generate smf pads
+export const smfWithoutParsing = (parameters: z.infer<typeof smf_def>) => {
+  const pads: AnySoupElement[] = []
+
+  for (let i = 1; i <= parameters.num_pins; i++) {
+    const { x, y } = getSmfCoords({
+      pn: i,
+      pad_spacing: Number.parseFloat(parameters.pad_spacing),
+    })
+    pads.push(
+      rectpad(
+        i,
+        x,
+        y,
+        Number.parseFloat(parameters.pl),
+        Number.parseFloat(parameters.pw),
+      ),
+    )
+  }
+  return pads
+}
