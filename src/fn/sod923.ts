9212dfaddd35ef15b2fa5249c6f57f581518010b
techmannih <manishchaudharyttt@gmail.com>
2025-02-10 03:08:31 +0530

    add in footprint

--- src/fn/sod923.ts
+++ src/fn/sod923.ts
@@ -11,7 +11,7 @@ export const sod_def = z.object({
   h: z.string().default("0.9mm"),
   pl: z.string().default("0.35mm"),
   pw: z.string().default("0.25mm"),
-  pad_spacing: z.string().default("1.2mm"),
+  p: z.string().default("1.2mm"),
 })
 
 export const sod923 = (
@@ -32,7 +32,7 @@ export const sod923 = (
     pcb_component_id: "",
     route: [
       {
-        x: length.parse(parameters.pad_spacing) / 2 + 0.15,
+        x: length.parse(parameters.p) / 2 + 0.15,
         y: length.parse(parameters.h) / 2,
       },
       {
@@ -44,7 +44,7 @@ export const sod923 = (
         y: -length.parse(parameters.h) / 2,
       },
       {
-        x: length.parse(parameters.pad_spacing) / 2 + 0.15,
+        x: length.parse(parameters.p) / 2 + 0.15,
         y: -length.parse(parameters.h) / 2,
       },
     ],
@@ -64,15 +64,15 @@ export const sod923 = (
 // Get coordinates for SOD pads
 export const getSodCoords = (parameters: {
   pn: number
-  pad_spacing: number
+  p: number
 }) => {
-  const { pn, pad_spacing } = parameters
+  const { pn, p } = parameters
 
   if (pn === 1) {
-    return { x: -pad_spacing / 2, y: 0 }
+    return { x: -p / 2, y: 0 }
     // biome-ignore lint/style/noUselessElse: <explanation>
   } else {
-    return { x: pad_spacing / 2, y: 0 }
+    return { x: p / 2, y: 0 }
   }
 }
 
@@ -83,7 +83,7 @@ export const sodWithoutParsing = (parameters: z.infer<typeof sod_def>) => {
   for (let i = 1; i <= parameters.num_pins; i++) {
     const { x, y } = getSodCoords({
       pn: i,
-      pad_spacing: Number.parseFloat(parameters.pad_spacing),
+      p: Number.parseFloat(parameters.p),
     })
     pads.push(
       rectpad(
