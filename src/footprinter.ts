29c34f6e38d1b97eefcab094b4a15327858df6bb
Rishikesh Maddhesiya <rishi89324@gmail.com>
2025-02-10 21:19:22 +0530

    improve TO-220

--- src/footprinter.ts
+++ src/footprinter.ts
@@ -58,6 +58,7 @@ export type Footprinter = {
   to220: () => FootprinterParamsBuilder<"w" | "h" | "p" | "id" | "od">
   sot235: () => FootprinterParamsBuilder<"h" | "p" | "pl" | "pw">
   to92: () => FootprinterParamsBuilder<"w" | "h" | "p" | "id" | "od">
+  minimelf: () => FootprinterParamsBuilder<"w" | "h" | "p" | "pw" | "pl">
   lqfp: (num_pins?: number) => FootprinterParamsBuilder<"w" | "h" | "pl" | "pw">
   pushbutton: () => FootprinterParamsBuilder<
     "tllabel" | "trlabel" | "bllabel" | "brlabel"
