90ebd75adc90303165623c5ae5c31a3e8f911ed8
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-19 23:19:37 +0530

    remove else statement

--- src/fn/sod323.ts
+++ src/fn/sod323.ts
@@ -68,12 +68,11 @@ export const getSodCoords = (parameters: {
 }) => {
   const { pn, p } = parameters
 
-  if (pn === 1) {
-    return { x: -p / 2, y: 0 }
-    // biome-ignore lint/style/noUselessElse: <explanation>
-  } else {
-    return { x: p / 2, y: 0 }
-  }
+if (pn === 1) {
+  return { x: -p / 2, y: 0 }
+}
+return { x: p / 2, y: 0 }
+
 }
 
 // Function to generate SOD pads
