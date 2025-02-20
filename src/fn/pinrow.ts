51408c6d028b138e122872d5f3f3267fa360e363
Anas sarkiz <72264380+AnasSarkiz@users.noreply.github.com>
2025-02-20 23:40:20 +0200

    Added male and female flag to pinrow footprint (#204)
    * Added male and female flag to pinrow footprint
    
    * male true only if female isn't defined
    
    * Added error message if male and female are both true

--- src/fn/pinrow.ts
+++ src/fn/pinrow.ts
@@ -3,13 +3,31 @@ import { length, type AnySoupElement } from "circuit-json"
 import { platedhole } from "../helpers/platedhole"
 import { silkscreenRef, type SilkscreenRef } from "src/helpers/silkscreenRef"
 
-export const pinrow_def = z.object({
-  fn: z.string(),
-  num_pins: z.number().optional().default(6),
-  p: length.default("0.1in").describe("pitch"),
-  id: length.default("1.0mm").describe("inner diameter"),
-  od: length.default("1.5mm").describe("outer diameter"),
-})
+export const pinrow_def = z
+  .object({
+    fn: z.string(),
+    num_pins: z.number().optional().default(6),
+    p: length.default("0.1in").describe("pitch"),
+    id: length.default("1.0mm").describe("inner diameter"),
+    od: length.default("1.5mm").describe("outer diameter"),
+    male: z.boolean().optional().describe("for male pin headers"),
+    female: z.boolean().optional().describe("for female pin headers"),
+  })
+  .transform((data) => ({
+    ...data,
+    male: data.male ?? (data.female ? false : true), // Auto-set male if not explicitly provided
+    female: data.female ?? false, // Default female to false if not provided
+  }))
+  .superRefine((data, ctx) => {
+    if (data.male && data.female) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message:
+          "'male' and 'female' cannot both be true it should be male or female.", //Error message if male and female are both true
+        path: ["male", "female"],
+      })
+    }
+  })
 
 export const pinrow = (
   raw_params: z.input<typeof pinrow_def>,
--- tests/pinrow.test.ts
+++ tests/pinrow.test.ts
@@ -5,5 +5,15 @@ import { fp } from "../src/footprinter"
 test("pinrow5", () => {
   const soup = fp.string("pinrow5").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const pinrowJson = fp.string("pinrow5_female").json()
+  expect(pinrowJson).toMatchObject({
+    fn: "pinrow",
+    num_pins: 5,
+    p: 2.54,
+    id: 1,
+    od: 1.5,
+    female: true,
+    male: false,
+  })
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "pinrow5")
 })
