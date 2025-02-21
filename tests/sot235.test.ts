9f0165f5790ccc6c3e63daf9074827ca5fa966cb
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-02-22 01:28:39 +0530

    fix: sot23_N number format fix (#212)
    * fix: sot23_N number format fix
    
    * formatbot: Automatically format code
    
    * remove the redundant format
    
    * formatbot: Automatically format code
    
    * remove test
    
    * fix build
    
    * build fix
    
    ---------
    
    Co-authored-by: tscircuitbot <tscircuitbot@users.noreply.github.com>

--- tests/sot235.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "bun:test"
-import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
-import { fp } from "../src/footprinter"
-
-test("sot235", () => {
-  const soup = fp.string("sot235").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot235")
-})
--- tests/sot236.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "bun:test"
-import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
-import { fp } from "../src/footprinter"
-
-test("sot236", () => {
-  const soup = fp.string("sot236").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot236")
-})
