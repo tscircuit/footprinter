5e970185dee97c9b5055043bc5cc4994a605960d
techmannih <manishchaudharyttt@gmail.com>
2025-02-22 22:03:30 +0530

    format

--- tests/pinrow.test.ts
+++ tests/pinrow.test.ts
@@ -5,17 +5,7 @@ import { fp } from "../src/footprinter"
 test("pinrow5", () => {
   const soup = fp.string("pinrow5").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
-  const pinrowJson = fp.string("pinrow5_female").json()
-  expect(pinrowJson).toMatchObject({
-    fn: "pinrow",
-    num_pins: 5,
-    p: 2.54,
-    id: 1,
-    od: 1.5,
-    female: true,
-    male: false,
-  })
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "pinrow5")
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "pinrow511")
 })
 
 test("pinrow4_rows2", () => {
