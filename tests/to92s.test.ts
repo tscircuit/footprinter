06d7caf838ae2997311ad2bd42e7d53b0fee165b
techmannih <manishchaudharyttt@gmail.com>
2025-02-28 17:05:45 +0530

    add to92s footprint

---
+++ tests/to92s.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("to92s", () => {
+  const circuitJson = fp.string("to92s").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path)
+})
+test("to92s_2", () => {
+  const circuitJson = fp.string("to92s_2").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "to92s_2")
+})
+
+test("to92s_3", () => {
+  const circuitJson = fp.string("to92s_3").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "to92s_3")
+})
