cd7f7019c0438ae75a69f71f26ae9e1f4c941cbe
techmannih <manishchaudharyttt@gmail.com>
2025-03-01 10:17:17 +0530

    add invalid test

--- tests/to92s.test.ts
+++ tests/to92s.test.ts
@@ -19,17 +19,14 @@ test("to92s_3", () => {
   const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "to92s_3")
 })
-
+// invalid test
 test("to92s_4", () => {
   try {
     const circuitJson = fp.string("to92s_4").circuitJson()
     const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
-    // Expecting failure due to invalid number of pins
     expect(svgContent).toMatchSvgSnapshot(import.meta.path, "to92s_4")
   } catch (error) {
-    // Assert that the error is of type Error
     const e = error as Error
-    // Catch the expected ZodError related to num_pins being invalid
     expect(e).toBeInstanceOf(Error)
     expect(e.message).toContain("Invalid literal value, expected 3")
     expect(e.message).toContain("Invalid literal value, expected 2")
