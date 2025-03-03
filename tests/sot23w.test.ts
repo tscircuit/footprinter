030835dab0fa944f7f930b4359fdb14ffebd11d6
techmannih <manishchaudharyttt@gmail.com>
2025-03-03 23:39:31 +0530

    add more footprint

--- tests/sot23w.test.ts
+++ tests/sot23w.test.ts
@@ -7,3 +7,17 @@ test("sot23w", () => {
   const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot23w")
 })
+
+test("sot23w", () => {
+  const circuitJson = fp.string("sot23w_pl1.2_pw0.9_p2_w5.2_h5").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(
+    import.meta.path,
+    "sot23w_pl1.2_pw0.9_p2_w5.2_h5",
+  )
+})
+test("sot23w", () => {
+  const circuitJson = fp.string("sot23w_p2_w5.1_h5.2").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot23w_p2_w5.1_h5.2")
+})
