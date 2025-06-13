9db49cb48546ac9ac53ef0995c6d34dc68fce567
techmannih <manishchaudharyttt@gmail.com>
2025-06-13 11:27:08 +0530

    add sot-6 footprint

--- src/footprinter.ts
+++ src/footprinter.ts
@@ -98,6 +98,7 @@ export type Footprinter = {
   sot563: () => FootprinterParamsBuilder<"w" | "p" | "pl" | "pw">
   sot723: () => FootprinterParamsBuilder<"w" | "h" | "pl" | "pw">
   sot23: () => FootprinterParamsBuilder<"w" | "h" | "pl" | "pw">
+  sot: () => FootprinterParamsBuilder<"w" | "h" | "p" | "pl" | "pw">
   sot323: () => FootprinterParamsBuilder<"w" | "h" | "p" | "pl" | "pw">
   sot89: () => FootprinterParamsBuilder<"w" | "p" | "pl" | "pw" | "h">
   sod323w: () => FootprinterParamsBuilder<"w" | "h" | "p" | "pl" | "pw">
