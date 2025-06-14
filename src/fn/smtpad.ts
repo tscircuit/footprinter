23e2d5bd988c14c65f919f9f10f30bf065d591c6
Andrii Balitskyi <10balian10@gmail.com>
2025-06-14 11:08:30 +0200

    Format fix

--- src/fn/smtpad.ts
+++ src/fn/smtpad.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { rectpad } from "../helpers/rectpad"
 import { circlepad } from "../helpers/circlepad"
 import { silkscreenRef } from "../helpers/silkscreenRef"
-import type { AnySoupElement } from "circuit-json"
+import type { AnyCircuitElement } from "circuit-json"
 import { mm } from "@tscircuit/mm"
 
 export const smtpad_def = z.object({
@@ -31,7 +31,7 @@ export type SmtPadDef = z.input<typeof smtpad_def>
 
 export const smtpad = (
   raw_params: SmtPadDef,
-): { circuitJson: AnySoupElement[]; parameters: any } => {
+): { circuitJson: AnyCircuitElement[]; parameters: any } => {
   const params = smtpad_def.parse(raw_params)
 
   let shape: "circle" | "rect" | "square" = "rect"
@@ -43,26 +43,26 @@ export const smtpad = (
     const d = params.d ?? params.pd ?? params.diameter
     const r = params.r ?? params.pr ?? params.radius
     const radius = r ? mm(r) : d ? mm(d) / 2 : mm("1mm") / 2
-    const circuitJson: AnySoupElement[] = [
+    const circuitJson: AnyCircuitElement[] = [
       circlepad(1, 0, 0, radius),
       silkscreenRef(0, radius + 0.5, 0.2),
     ]
     return { circuitJson, parameters: { ...params, shape, radius } }
-  } else {
-    let w = params.w ?? params.pw ?? params.width ?? params.s ?? params.size
-    let h = params.h ?? params.ph ?? params.height
-    let width = w ? mm(w) : mm("1mm")
-    let height = h ? mm(h) : undefined
-
-    if (shape === "square") {
-      if (height === undefined) height = width
-    } else {
-      if (height === undefined) height = width
-    }
-    const circuitJson: AnySoupElement[] = [
-      rectpad(1, 0, 0, width, height!),
-      silkscreenRef(0, height! / 2 + 0.5, 0.2),
-    ]
-    return { circuitJson, parameters: { ...params, shape, width, height } }
   }
+
+  const w = params.w ?? params.pw ?? params.width ?? params.s ?? params.size
+  const h = params.h ?? params.ph ?? params.height
+  const width = w ? mm(w) : mm("1mm")
+  let height = h ? mm(h) : undefined
+
+  if (shape === "square") {
+    if (height === undefined) height = width
+  } else {
+    if (height === undefined) height = width
+  }
+  const circuitJson: AnyCircuitElement[] = [
+    rectpad(1, 0, 0, width, height!),
+    silkscreenRef(0, height! / 2 + 0.5, 0.2),
+  ]
+  return { circuitJson, parameters: { ...params, shape, width, height } }
 }
