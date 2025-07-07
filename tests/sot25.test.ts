a143c4f85a28a76a73d21189d9c77705d2960350
Abimael Martell <abimex@gmail.com>
2025-07-07 11:14:24 -0700

    Add sot25 footprint

---
+++ tests/sot25.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("sot25", () => {
+  const circuitJson = fp.string("sot25").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot25")
+})
