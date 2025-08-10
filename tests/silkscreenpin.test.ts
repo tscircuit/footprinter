78333dfe6d0f484a19f51184f480590d82efd4e6
techmannih <manishchaudharyttt@gmail.com>
2025-08-10 21:43:46 +0530

    format code

--- tests/silkscreenpin.test.ts
+++ tests/silkscreenpin.test.ts
@@ -2,11 +2,23 @@ import { test, expect } from "bun:test"
 import { silkscreenPin } from "../src/helpers/silkscreenPin"
 
 test("silkscreenPin anchorplacement top uses bottom_center alignment", () => {
-  const res = silkscreenPin({ fs: 1, pn: 1, anchor_x: 0, anchor_y: 0, anchorplacement: "top" })
+  const res = silkscreenPin({
+    fs: 1,
+    pn: 1,
+    anchor_x: 0,
+    anchor_y: 0,
+    anchorplacement: "top",
+  })
   expect(res.anchor_alignment).toBe("bottom_center")
 })
 
 test("silkscreenPin anchorplacement bottom uses top_center alignment", () => {
-  const res = silkscreenPin({ fs: 1, pn: 1, anchor_x: 0, anchor_y: 0, anchorplacement: "bottom" })
+  const res = silkscreenPin({
+    fs: 1,
+    pn: 1,
+    anchor_x: 0,
+    anchor_y: 0,
+    anchorplacement: "bottom",
+  })
   expect(res.anchor_alignment).toBe("top_center")
 })
