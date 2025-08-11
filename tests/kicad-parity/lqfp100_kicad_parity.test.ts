9d15dbc233cf62fe4674e0ea86b1752db03f4423
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-08-11 19:47:49 +0200

    Added LQFP-100-1EP_14x14mm_P0.5mm_EP6.9x6.9mm parity test (#357)

---
+++ tests/kicad-parity/lqfp100_kicad_parity.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "bun:test"
+import { compareFootprinterVsKicad } from "../fixtures/compareFootprinterVsKicad"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+
+test("parity/lqfp100", async () => {
+  const { avgRelDiff, combinedFootprintElements, booleanDifferenceSvg } =
+    await compareFootprinterVsKicad(
+      "lqfp100_w14_p0.5mm_pw0.3_pl1.475_thermalpad6.9x6.9",
+      "Package_QFP.pretty/LQFP-100-1EP_14x14mm_P0.5mm_EP6.9x6.9mm.circuit.json",
+    )
+
+  const svgContent = convertCircuitJsonToPcbSvg(combinedFootprintElements)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "lqfp100")
+  expect(booleanDifferenceSvg).toMatchSvgSnapshot(
+    import.meta.path,
+    "lqfp100_boolean_difference",
+  )
+})
