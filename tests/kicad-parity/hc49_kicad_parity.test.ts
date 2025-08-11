abb597551b4d953abc45c2685d6feb52938c8c96
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-08-11 15:05:14 +0200

    Added Crystal.pretty/Crystal_HC49-4H_Vertical parity test

---
+++ tests/kicad-parity/hc49_kicad_parity.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "bun:test"
+import { compareFootprinterVsKicad } from "../fixtures/compareFootprinterVsKicad"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+
+test("parity/hc49", async () => {
+  const { avgRelDiff, combinedFootprintElements, booleanDifferenceSvg } =
+    await compareFootprinterVsKicad(
+      "hc49_id0.8mm_od1.5mm",
+      "Crystal.pretty/Crystal_HC49-4H_Vertical.circuit.json",
+    )
+
+  const svgContent = convertCircuitJsonToPcbSvg(combinedFootprintElements)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "hc49")
+  expect(booleanDifferenceSvg).toMatchSvgSnapshot(
+    import.meta.path,
+    "hc49_boolean_difference",
+  )
+})
