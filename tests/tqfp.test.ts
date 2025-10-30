3cab00414f321776f3c5ae47d602ade3264cdc27
nailoo <rahulsinghnailoo@gmail.com>
2025-10-31 00:28:53 +0530

    update

--- tests/tqfp.test.ts
+++ tests/tqfp.test.ts
@@ -2,8 +2,11 @@ import { test, expect } from "bun:test"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
-test("tqfp64", () => {
-  const soup = fp.string("tqfp64").circuitJson()
+test("tqfp64_w10_p0.5mm_pw0.3_pl1.475mm", () => {
+  const soup = fp.string("tqfp64_w10_p0.5mm_pw0.3_pl1.475mm").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "tqfp64")
+  expect(svgContent).toMatchSvgSnapshot(
+    import.meta.path,
+    "tqfp64_w10_p0.5mm_pw0.3_pl1.475mm",
+  )
 })
