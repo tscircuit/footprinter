bf0fef38c09fd331441a51315a9a805b34a0ee9c
rushabhcodes <mail@rushabh.dev>
2025-11-02 17:47:15 +0530

    refactor: rename variable for clarity in WSON tests

--- tests/wson.test.ts
+++ tests/wson.test.ts
@@ -3,20 +3,20 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
 test("wson6", () => {
-  const soup = fp.string("wson6").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const circuitJson = fp.string("wson6").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "wson6")
 })
 
 test("wson6_ep", () => {
-  const soup = fp.string("wson6_ep").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const circuitJson = fp.string("wson6_ep").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "wson6_ep")
 })
 
 test("wson8_2x2mm_p0.5mm_ep0.9x1.6mm", () => {
-  const soup = fp.string("wson8_2x2mm_p0.5mm_ep0.9x1.6mm").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const circuitJson = fp.string("wson8_2x2mm_p0.5mm_ep0.9x1.6mm").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(
     import.meta.path,
     "wson8_2x2mm_p0.5mm_ep0.9x1.6mm",
