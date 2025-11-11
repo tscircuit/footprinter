0beb2861ec95b16c0449b48feef169a0a9c0ff3a
Rahul Nailoo <rahulsinghnailoo@gmail.com>
2025-11-11 08:09:36 +0530

    Fix QFN pad width calculation (#415)
    * Fix QFN pad width calculation
    
    * add test
    
    * add test

---
+++ tests/kicad-parity/qfn32_kicad_parity.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "bun:test"
+import { compareFootprinterVsKicad } from "../fixtures/compareFootprinterVsKicad"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+
+test("parity/qfn32_thermalpad3.1x3.1mm", async () => {
+  const { avgRelDiff, combinedFootprintElements, booleanDifferenceSvg } =
+    await compareFootprinterVsKicad(
+      "qfn32_thermalpad3.1x3.1mm",
+      "Package_DFN_QFN.pretty/QFN-32-1EP_5x5mm_P0.5mm_EP3.1x3.1mm.circuit.json",
+    )
+
+  const svgContent = convertCircuitJsonToPcbSvg(combinedFootprintElements)
+  expect(svgContent).toMatchSvgSnapshot(
+    import.meta.path,
+    "qfn32_thermalpad3.1x3.1mm",
+  )
+  expect(booleanDifferenceSvg).toMatchSvgSnapshot(
+    import.meta.path,
+    "qfn32_thermalpad3.1x3.1mm_boolean_difference",
+  )
+})
--- tests/qfn.test.ts
+++ tests/qfn.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from "bun:test"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
-test("qfn16_w4_h4_p0.65mm", () => {
-  const soup = fp.string("qfn16_w4_h4_p0.65mm").circuitJson()
+test("qfn32", () => {
+  const soup = fp.string("qfn32").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "qfn16_w4_h4_p0.65mm")
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "qfn32")
 })
