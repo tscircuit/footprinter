1cdcb3daf9c5c3c766d10ebc4602bdc4452e8613
Raghav Arora <142147483+RaghavArora14@users.noreply.github.com>
2025-11-16 15:36:51 +0530

    Fix test to use correct circuitJson for SVG conversion

--- tests/sot886.test.ts
+++ tests/sot886.test.ts
@@ -4,6 +4,6 @@ import { fp } from "../src/footprinter"
 
 test("sot886", () => {
   const circuitJson = fp.string("sot886").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(soup)
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot886")
 })
