c641dd8b2f81dd156b18c9f63fcb7dedcf40aaef
Raghav Arora <142147483+RaghavArora14@users.noreply.github.com>
2025-11-16 15:35:20 +0530

    Apply suggestion from @techmannih
    Co-authored-by: Manish chaudhary  <125847751+techmannih@users.noreply.github.com>

--- tests/sot886.test.ts
+++ tests/sot886.test.ts
@@ -3,7 +3,7 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
 test("sot886", () => {
-  const soup = fp.string("sot886").circuitJson()
+  const circuitJson = fp.string("sot886").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "sot886")
 })
