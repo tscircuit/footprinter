dc621f16e96a838b43ad80001ec07c7c8c8549c6
Shehab Ahmed <shehab924617940@gmail.com>
2025-12-14 15:18:29 +0200

    improve the pitch and the pads and remove underscore

--- src/fn/radial.ts
+++ src/fn/radial.ts
@@ -10,12 +10,13 @@ import { base_def } from "../helpers/zod/base_def"
 
 export const radial_def = base_def.extend({
   fn: z.string(),
-  p: length.optional().default("1.6mm"),
-  id: length.optional().default("0.7mm"),
-  od: length.optional().default("1.25mm"),
 
-  body_d: length.optional().default("5.0mm"),
-  body_h: length.optional().default("12.5mm"),
+  p: length.optional().default("1.53mm"),
+  id: length.optional().default("0.6mm"),
+  od: length.optional().default("1.2mm"),
+
+  bodyDiameter: length.optional().default("5.0mm"),
+  bodyHeight: length.optional().default("12.5mm"),
 })
 
 export type RadialDef = z.input<typeof radial_def>
@@ -24,34 +25,48 @@ export const radial = (
   raw_params: RadialDef,
 ): { circuitJson: AnySoupElement[]; parameters: any } => {
   const parameters = radial_def.parse(raw_params)
-  const { p, id, od, body_d, body_h } = parameters
+
+  const { p, id, od, bodyDiameter, bodyHeight } = parameters
 
   const plated_holes = [
     platedhole(1, -p / 2, 0, id, od),
     platedhole(2, p / 2, 0, id, od),
   ]
 
-  const silkscreenLine: PcbSilkscreenPath = {
-    type: "pcb_silkscreen_path",
-    layer: "top",
-    pcb_component_id: "",
-    route: [
-      { x: -od / 2, y: -od / 2 },
-      { x: od / 2, y: -od / 2 },
-      { x: od / 2, y: od / 2 },
-      { x: -od / 2, y: od / 2 },
-      { x: -od / 2, y: -od / 2 },
-    ],
-    stroke_width: 0.1,
-    pcb_silkscreen_path_id: "",
-  }
+  const silkscreenLines: PcbSilkscreenPath[] = [
+    // Top line
+    {
+      type: "pcb_silkscreen_path",
+      layer: "top",
+      pcb_component_id: "",
+      route: [
+        { x: -od / 2, y: -od / 2 - 0.07 },
+        { x: od / 2, y: -od / 2 - 0.07 },
+      ],
+      stroke_width: 0.1,
+      pcb_silkscreen_path_id: "",
+    },
+
+    // Bottom line
+    {
+      type: "pcb_silkscreen_path",
+      layer: "top",
+      pcb_component_id: "",
+      route: [
+        { x: -od / 2, y: od / 2 + 0.07 },
+        { x: od / 2, y: od / 2 + 0.07 },
+      ],
+      stroke_width: 0.1,
+      pcb_silkscreen_path_id: "",
+    },
+  ]
 
   const silkscreenRefText: SilkscreenRef = silkscreenRef(0, od / 2 + 1, 0.5)
 
   return {
     circuitJson: [
       ...plated_holes,
-      silkscreenLine,
+      ...silkscreenLines,
       silkscreenRefText as AnySoupElement,
     ],
 
@@ -62,8 +77,8 @@ export const radial = (
 
       model_3d: {
         body_shape: "cylinder",
-        body_diameter: body_d,
-        body_height: body_h,
+        body_diameter: bodyDiameter,
+        body_height: bodyHeight,
         orientation: "vertical",
         pin_count: 2,
         lead_pitch: p,
