1a6f8b73b16ef9b0daf3b36cf36044c80c40b4f1
Juste-Leo <leonard.adamo66@gmail.com>
2025-12-15 22:17:59 +0100

    style: Format code

--- tests/gbj.test.ts
+++ tests/gbj.test.ts
@@ -3,9 +3,9 @@ import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
 test("gbj standard footprint", () => {
-	const circuitJson = fp.string("gbj").circuitJson()
+  const circuitJson = fp.string("gbj").circuitJson()
 
-	const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
 
-	expect(svgContent).toMatchSvgSnapshot(import.meta.path + "gbj")
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path + "gbj")
 })
