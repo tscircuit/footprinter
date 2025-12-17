c6edb3c2787ed6b726b065198e187f033571aaaa
Juste-Leo <leonard.adamo66@gmail.com>
2025-12-17 22:39:51 +0100

    added ref and other corrections

--- src/fn/to92l.ts
+++ src/fn/to92l.ts
@@ -1,31 +1,35 @@
-import { type AnySoupElement, type PcbSilkscreenPath } from "circuit-json"
 import { z } from "zod"
 import { platedhole } from "src/helpers/platedhole"
 import { platedHoleWithRectPad } from "src/helpers/platedHoleWithRectPad"
+import type { AnyCircuitElement, PcbSilkscreenPath } from "circuit-json"
+import { silkscreenRef, type SilkscreenRef } from "src/helpers/silkscreenRef"
 import { base_def } from "src/helpers/zod/base_def"
 
 export const to92l_def = base_def.extend({
   fn: z.string().default("to92l"),
-  drill: z.number().default(0.75),
-  pw: z.number().default(1.3),
-  pl: z.number().default(1.3),
+  p: z.string().default("1.27mm"),
+  id: z.string().default("0.75mm"),
+  od: z.string().default("1.3mm"),
 })
 
 export const to92l = (
   raw_params: z.input<typeof to92l_def>,
-): { circuitJson: AnySoupElement[]; parameters: any } => {
-  const params = to92l_def.parse(raw_params)
-  const circuitJson: AnySoupElement[] = []
+): { circuitJson: AnyCircuitElement[]; parameters: any } => {
+  const parameters = to92l_def.parse(raw_params)
+  const circuitJson: AnyCircuitElement[] = []
+
+  const p = Number.parseFloat(parameters.p)
 
   circuitJson.push(
-    platedHoleWithRectPad(1, 0, 0, params.drill, params.pw, params.pl),
+    platedHoleWithRectPad(1, 0, 0, parameters.id, parameters.od, parameters.od),
   )
 
-  circuitJson.push(platedhole(2, 1.28, 1.27, params.drill, params.pw))
-  circuitJson.push(platedhole(3, 2.54, 0, params.drill, params.pw))
+  circuitJson.push(platedhole(2, p, p, parameters.id, parameters.od))
+
+  circuitJson.push(platedhole(3, p * 2, 0, parameters.id, parameters.od))
 
   const radius = 2.4
-  const cx = 1.27
+  const cx = p
   const cy = 0.2
 
   const semicircle = Array.from({ length: 32 }, (_, i) => {
@@ -52,5 +56,12 @@ export const to92l = (
 
   circuitJson.push(silkBody)
 
-  return { circuitJson, parameters: params }
+  const silkscreenRefText: SilkscreenRef = silkscreenRef(
+    cx,
+    cy + radius + 1,
+    0.5,
+  )
+  circuitJson.push(silkscreenRefText as AnyCircuitElement)
+
+  return { circuitJson, parameters }
 }
