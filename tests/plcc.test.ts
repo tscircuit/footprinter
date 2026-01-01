9135dee76037e372d817bd8b9b6c7272648a747a
Sahil Gupta <guptas3067@gmail.com>
2026-01-01 21:39:22 +0530

    format

--- tests/plcc.test.ts
+++ tests/plcc.test.ts
@@ -5,5 +5,8 @@ import { fp } from "../src/footprinter"
 test("plcc44_w16.5_h16.5_p1.27mm", () => {
   const soup = fp.string("plcc44_w16.5_h16.5_p1.27mm").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "plcc44_w16.5_h16.5_p1.27mm")
+  expect(svgContent).toMatchSvgSnapshot(
+    import.meta.path,
+    "plcc44_w16.5_h16.5_p1.27mm",
+  )
 })
