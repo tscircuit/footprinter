30f701363b89251d29187186a35b100e18a3e828
techmannih <manishchaudharyttt@gmail.com>
2026-01-05 12:30:49 +0530

    add 0402_x4 kicad test

--- src/helpers/res0402-array4.ts
+++ src/helpers/res0402-array4.ts
@@ -1,10 +1,10 @@
 import type { AnyCircuitElement } from "circuit-json"
 import { chipArray } from "./chipArray"
 
-const padSpacing = 1.02
-const padWidth = 0.54
-const padHeight = 0.64
-const padPitch = 0.9
+const padSpacing = 1.0 // Horizontal spacing between columns (KiCad: 1.0mm)
+const padWidth = 0.5 // Pad width (KiCad: 0.5mm)
+const padHeight = 0.32 // Pad height (KiCad: 0.32mm)
+const padPitch = 0.5 // Vertical pitch between pads (KiCad: 0.5mm)
 
 export const res0402Array4 = (params: {
   textbottom?: boolean
