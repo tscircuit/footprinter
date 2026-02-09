9170a72253c7f1e6fb40d681d208d499d2572639
Rushabh Patil <mail@rushabh.dev>
2026-02-09 17:45:48 +0530

    feat: enhance SOP-8 footprint definition and add tests for KiCad parity (#430)
    * feat: enhance SOP-8 footprint definition and add tests for KiCad parity
    
    * fix: update silkscreen reference position for SOP-8 footprint
    
    * fix: adjust silkscreen reference positions in SOP-8 footprint and update snapshots

--- src/fn/sop8.ts
+++ src/fn/sop8.ts
@@ -1,15 +1,20 @@
-import type { AnySoupElement, PcbSilkscreenPath } from "circuit-json"
+import type { AnyCircuitElement, PcbSilkscreenPath } from "circuit-json"
 import { extendSoicDef, type SoicInput, getCcwSoicCoords } from "./soic"
 import { rectpad } from "src/helpers/rectpad"
 import { type SilkscreenRef, silkscreenRef } from "src/helpers/silkscreenRef"
 
-export const sop8_def = extendSoicDef({})
+export const sop8_def = extendSoicDef({
+  w: "7.05mm",
+  p: "1.27mm",
+  pw: "0.65mm",
+  pl: "1.975mm",
+})
 
 export const sop8 = (
   raw_params: SoicInput,
-): { circuitJson: AnySoupElement[]; parameters: any } => {
+): { circuitJson: AnyCircuitElement[]; parameters: any } => {
   const parameters = sop8_def.parse(raw_params)
-  const pads: AnySoupElement[] = []
+  const pads: AnyCircuitElement[] = []
 
   for (let i = 0; i < parameters.num_pins; i++) {
     const { x, y } = getCcwSoicCoords({
@@ -26,11 +31,7 @@ export const sop8 = (
   }
 
   const sh = (parameters.num_pins / 2 - 1) * parameters.p + parameters.pw
-  const silkscreenRefText: SilkscreenRef = silkscreenRef(
-    0,
-    sh / 2 - 0.5,
-    sh / 12,
-  )
+  const silkscreenRefText: SilkscreenRef = silkscreenRef(0, sh / 2 + 1, sh / 12)
 
   const silkscreenLine: PcbSilkscreenPath = {
     layer: "top",
@@ -38,8 +39,8 @@ export const sop8 = (
     pcb_silkscreen_path_id: "",
     type: "pcb_silkscreen_path",
     route: [
-      { x: -parameters.w / 3, y: sh / 2 + 0.2 },
-      { x: parameters.w / 3, y: sh / 2 + 0.2 },
+      { x: -parameters.w / 3, y: sh / 2 + 0.4 },
+      { x: parameters.w / 3, y: sh / 2 + 0.4 },
     ],
     stroke_width: 0.1,
   }
@@ -49,7 +50,7 @@ export const sop8 = (
       ...pads,
       silkscreenRefText,
       silkscreenLine,
-    ] as AnySoupElement[],
+    ] as AnyCircuitElement[],
     parameters,
   }
 }
