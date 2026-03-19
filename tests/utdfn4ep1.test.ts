6541a80d4e67935a7f0d11ca558795a6a00658ce
victorjzq <118996546+victorjzq@users.noreply.github.com>
2026-03-19 18:52:14 +0700

    test: split utdfn4ep tests into separate files per project convention

---
+++ tests/utdfn4ep1.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("utdfn4ep", () => {
+  const circuitJson = fp.string("utdfn4ep").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "utdfn4ep")
+})
--- tests/utdfn4ep2.test.ts
+++ tests/utdfn4ep2.test.ts
@@ -2,12 +2,6 @@ import { test, expect } from "bun:test"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { fp } from "../src/footprinter"
 
-test("utdfn4ep", () => {
-  const circuitJson = fp.string("utdfn4ep").circuitJson()
-  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
-  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "utdfn4ep")
-})
-
 test("utdfn4ep_pw0.3mm_ph0.5mm_epw0.55mm_eph0.45mm", () => {
   const circuitJson = fp
     .string("utdfn4ep_pw0.3mm_ph0.5mm_epw0.55mm_eph0.45mm")
