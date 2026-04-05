bb99d49f2bbf8c6ddc36b42bb7bce57a33ae676f
Aslan Aliyev <aliyevr889@gmail.com>
2026-04-05 06:21:53 +0500

    Update string-parser.test.ts

--- tests/string-parser.test.ts
+++ tests/string-parser.test.ts
@@ -2,12 +2,5 @@ import { test, expect } from "bun:test"
 import { fp } from "../src/footprinter"
 
 test("string builder ignores empty segments", () => {
-  expect(() => fp.string("0603__").circuitJson()).not.toThrow()
-})
-
-test("string builder works for led and diode imperial sizes", () => {
-  expect(() => fp.string("led0402").circuitJson()).not.toThrow()
-  expect(() => fp.string("led0603").circuitJson()).not.toThrow()
-  expect(() => fp.string("diode0402").circuitJson()).not.toThrow()
-  expect(() => fp.string("diode0805").circuitJson()).not.toThrow()
+    expect(() => fp.string("0603__").circuitJson()).not.toThrow()
 })
