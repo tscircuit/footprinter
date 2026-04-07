902c99740ccf27bdf0f628be3201c566c74f98be
Mustafa Mulla <mustafamulla765@gmail.com>
2026-04-07 12:05:54 +0530

    upd

---
+++ tests/kicad-parity/smb_kicad_parity.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "bun:test"
+import { compareFootprinterVsKicad } from "../fixtures/compareFootprinterVsKicad"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+
+test("parity/smb", async () => {
+  const {
+    avgRelDiff,
+    combinedFootprintElements,
+    booleanDifferenceSvg,
+    courtyardDiffPercent,
+  } = await compareFootprinterVsKicad(
+    "smb",
+    "Diode_SMD.pretty/D_SMB.circuit.json",
+  )
+
+  const svgContent = convertCircuitJsonToPcbSvg(combinedFootprintElements, {
+    showCourtyards: true,
+  })
+  expect(courtyardDiffPercent).toBeLessThan(5)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "smb")
+  expect(booleanDifferenceSvg).toMatchSvgSnapshot(
+    import.meta.path,
+    "smb_boolean_difference",
+  )
+  expect(avgRelDiff).toBeLessThan(0.05)
+})
