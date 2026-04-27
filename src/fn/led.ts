946c725ae0e7300dc1fd31cfdbb35eaa0e0b8a97
Shibo <shibosoftwaredev@gmail.com>
2026-04-27 17:56:37 +0200

    reverse polarity of the silkscreen in led components (#607)

--- src/fn/led.ts
+++ src/fn/led.ts
@@ -1,8 +1,17 @@
-import type { AnySoupElement } from "circuit-json"
+import type { AnySoupElement, PcbSilkscreenPath } from "circuit-json"
 import { type PassiveDef, passive } from "../helpers/passive-fn"
 
 export const led = (
   parameters: PassiveDef,
 ): { circuitJson: AnySoupElement[]; parameters: PassiveDef } => {
-  return { circuitJson: passive(parameters), parameters }
+  const circuitJson = passive(parameters).map((element) => {
+    if (element.type !== "pcb_silkscreen_path") return element
+
+    return {
+      ...(element as PcbSilkscreenPath),
+      route: element.route.map((point) => ({ ...point, x: -point.x })),
+    }
+  })
+
+  return { circuitJson, parameters }
 }
