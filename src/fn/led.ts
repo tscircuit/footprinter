b983609a62e56040fec29fa5d2b48212c23a2553
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-04-27 17:57:22 +0200

    Revert "reverse polarity of the silkscreen in led components (#607)"
    This reverts commit 946c725ae0e7300dc1fd31cfdbb35eaa0e0b8a97.

--- src/fn/led.ts
+++ src/fn/led.ts
@@ -1,17 +1,8 @@
-import type { AnySoupElement, PcbSilkscreenPath } from "circuit-json"
+import type { AnySoupElement } from "circuit-json"
 import { type PassiveDef, passive } from "../helpers/passive-fn"
 
 export const led = (
   parameters: PassiveDef,
 ): { circuitJson: AnySoupElement[]; parameters: PassiveDef } => {
-  const circuitJson = passive(parameters).map((element) => {
-    if (element.type !== "pcb_silkscreen_path") return element
-
-    return {
-      ...(element as PcbSilkscreenPath),
-      route: element.route.map((point) => ({ ...point, x: -point.x })),
-    }
-  })
-
-  return { circuitJson, parameters }
+  return { circuitJson: passive(parameters), parameters }
 }
