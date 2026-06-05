4d819f75432a3cf1339375b0f8ec1bc8a347636e
Ojaswee Upadhyay <ojaswee2095@gmail.com>
2026-06-05 21:19:15 +0530

    feat: add support for lpcc footprint (LPCC-32)
    Implemented the \lpcc\ footprint (Leadless Plastic Chip Carrier) which is effectively an alias to \qfn\ but with an exposed thermal pad enabled by default, as requested in issue #300. Added test coverage for \lpcc32_w5mm_p0.5mm\.
    
    Closes #300

---
+++ tests/lpcc.test.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "bun:test"
+import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
+import { fp } from "../src/footprinter"
+
+test("lpcc32", () => {
+  const circuitJson = fp.string("lpcc32_w5mm_p0.5mm").circuitJson()
+  const svgContent = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svgContent).toMatchSvgSnapshot(import.meta.path, "lpcc32")
+})
