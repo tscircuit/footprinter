a069fc61576c621b343cbf4de1a2ad0471eb8012
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2026-07-05 12:07:24 +0530

    Add fabrication note support to all diode footprints (#694)

--- src/fn/smc.ts
+++ src/fn/smc.ts
@@ -8,6 +8,7 @@ import { rectpad } from "../helpers/rectpad"
 import { silkscreenRef, type SilkscreenRef } from "src/helpers/silkscreenRef"
 import { length } from "circuit-json"
 import { base_def } from "../helpers/zod/base_def"
+import { createFabricationNoteDiodeFromCopperPads } from "../helpers/create-fabrication-note-diode"
 
 export const smc_def = base_def.extend({
   fn: z.string(),
@@ -67,6 +68,7 @@ export const smc = (
 
   return {
     circuitJson: smcWithoutParsing(parameters).concat(
+      ...createFabricationNoteDiodeFromCopperPads(parameters),
       silkscreenLine as AnyCircuitElement,
       silkscreenRefText as AnyCircuitElement,
       courtyard as AnyCircuitElement,
