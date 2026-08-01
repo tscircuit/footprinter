25c5e1f5f3f2e689a85bc0cfc9bb754afcbefbc7
Krishna Raj B <bkr.2007@yahoo.com>
2026-08-01 12:56:42 +0530

    test: update tsop footprint string to perfectly align with kicad

--- tests/kicad-parity/tsop48_kicad_parity.test.ts
+++ tests/kicad-parity/tsop48_kicad_parity.test.ts
@@ -2,22 +2,31 @@ import { expect, test } from "bun:test"
 import { compareFootprinterVsKicad } from "../fixtures/compareFootprinterVsKicad"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 
-test("parity/tsop48_w18.4_h12.4_p0.5mm", async () => {
-  const { avgRelDiff, combinedFootprintElements, booleanDifferenceSvg } =
-    await compareFootprinterVsKicad(
-      "tsop48_w18.4_h12.4_p0.5mm",
-      "Package_SO.pretty/TSOP-I-48_18.4x12mm_P0.5mm.circuit.json",
-    )
+test("parity/tsop48_w17.95_h12.4_p0.5mm_pl1.575_pw0.3", async () => {
+  const {
+    avgRelDiff,
+    courtyardDiffPercent,
+    combinedFootprintElements,
+    booleanDifferenceSvg,
+  } = await compareFootprinterVsKicad(
+    "tsop48_w17.95_h12.4_p0.5mm_pl1.575_pw0.3",
+    "Package_SO.pretty/TSOP-I-48_18.4x12mm_P0.5mm.circuit.json",
+  )
 
   const svgContent = convertCircuitJsonToPcbSvg(combinedFootprintElements, {
     showCourtyards: true,
   })
+
+  // The pads should be perfectly aligned now
+  expect(avgRelDiff).toBeLessThan(0.05)
+  expect(courtyardDiffPercent).toBeLessThan(10)
+
   expect(svgContent).toMatchSvgSnapshot(
     import.meta.path,
-    "tsop48_w18.4_h12.4_p0.5mm",
+    "tsop48_w17.95_h12.4_p0.5mm_pl1.575_pw0.3",
   )
   expect(booleanDifferenceSvg).toMatchSvgSnapshot(
     import.meta.path,
-    "tsop48_w18.4_h12.4_p0.5mm_boolean_difference",
+    "tsop48_w17.95_h12.4_p0.5mm_pl1.575_pw0.3_boolean_difference",
   )
 })
