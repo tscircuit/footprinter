5e8fd670529c7ce87a21bb6537dfdee445ad57cb
Krishna Raj B <bkr.2007@yahoo.com>
2026-08-01 11:23:06 +0530

    style: run formatter

--- tests/lcc.test.ts
+++ tests/lcc.test.ts
@@ -5,9 +5,9 @@ import { fp } from "../src/footprinter"
 test("lcc68", () => {
   const soup = fp.string("lcc68_w24.2_h24.2_p1.27mm").circuitJson()
   const svgContent = convertCircuitJsonToPcbSvg(soup)
-  
+
   const pads = soup.filter((element) => element.type === "pcb_smtpad")
   expect(pads).toHaveLength(68)
-  
+
   expect(svgContent).toMatchSvgSnapshot(import.meta.path, "lcc68")
 })
