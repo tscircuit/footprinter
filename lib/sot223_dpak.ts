2767635d9fe2a169654c35808ad3f67981f62248
Rodrigoue9 <devhq.oficial@gmail.com>
2026-09-16 09:29:37 -0300

    style: format code with biome

--- lib/sot223_dpak.ts
+++ lib/sot223_dpak.ts
@@ -1,3 +1,8 @@
 export function generateSot223Pads() {
-  return [{ pad: 1, x: -2.3, y: -3.0 }, { pad: 2, x: 0, y: -3.0 }, { pad: 3, x: 2.3, y: -3.0 }, { pad: 4, x: 0, y: 3.0 }];
+  return [
+    { pad: 1, x: -2.3, y: -3.0 },
+    { pad: 2, x: 0, y: -3.0 },
+    { pad: 3, x: 2.3, y: -3.0 },
+    { pad: 4, x: 0, y: 3.0 },
+  ]
 }
