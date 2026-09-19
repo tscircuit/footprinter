24d169fa08aae7fba7d923bf1da12620e2a00a27
halc8312 <tatsuki.n.0605@gmail.com>
2026-09-19 17:54:20 +0900

    update src/footprinter.ts for utdfn

--- src/footprinter.ts
+++ src/footprinter.ts
@@ -650,6 +650,7 @@ const normalizeDefinition = (def: string): string => {
     .replace(/^sot-223-(\d+)(?=_|$)/i, "sot223_$1")
     .replace(/^to-220f-(\d+)(?=_|$)/i, "to220f_$1")
     .replace(/^jst_(ph|sh|zh|xh)_(\d+)(?=_|$)/i, "jst$2_$1")
+    .replace(/^u?t?dfn-(\d+)-ep(?:\([^)]*\))?(?=_|$)/i, "utdfn$1ep")
 }
 
 const normalizeMicrometerLengths = (value: string): string =>
